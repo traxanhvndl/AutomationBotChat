7b9bb43e47803cf7bb093c22a83b59e9b311f7f3
traxanhvndl <dangnguyennguyenvu@gmail.com>
2017-07-06 13:37:07 +0700

    Vu

--- AutomationBotChat/app.js
+++ AutomationBotChat/app.js
@@ -1,16 +1,19 @@
 var cloudTopic = require("./lib/cloudTalk");
 var express = require('express'),
-	app = express(),
-	server = require('http').createServer(app),
-	io = require('socket.io').listen(server),
-	users = {},
-	nodemailer = require("nodemailer");
-	session_topic = {};
-var	user_list = {};
-	user_data = {};
-    // port = process.env.PORT || 5000,
-	// ip = process.env.HOST || '192.168.35.44';
-    // ip = process.env.HOST || '11.11.254.69';
+    bodyParser = require('body-parser'),
+    mysql = require('mysql'),
+    fs = require('fs'),
+    app = express(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server),
+    users = {},
+    nodemailer = require("nodemailer"),
+    session_topic = {},
+    user_list = {},
+    user_data = {};
+// port = process.env.PORT || 5000,
+// ip = process.env.HOST || '192.168.35.44';
+// ip = process.env.HOST || '11.11.254.69';
 
 var port = process.env.PORT || 3000;
 var ip = process.env.HOST || '0.0.0.0';
@@ -34,17 +37,17 @@ var transporter = nodemailer.createTransport({
     }
 });
 
-server.listen(port, ip, function(){
-    console.log("Server is running on ["+ip+":"+port+"]")
+server.listen(port, ip, function() {
+    console.log("Server is running on [" + ip + ":" + port + "]")
 });
 app.use(express.static(__dirname + '/public'));
-app.get('/', function(req, res){
-	console.log('User connected !')
+app.get('/', function(req, res) {
+    console.log('User connected !')
 });
 
-app.get('/', function(req, res){
-	console.log('Connected !')
-	res.sendfile(__dirname + '/public/admin.html');
+app.get('/', function(req, res) {
+    console.log('Connected !')
+    res.sendfile(__dirname + '/public/admin.html');
 });
 
 app.get('/sendmail', function(req, res) {
@@ -69,150 +72,207 @@ app.get('/sendmail', function(req, res) {
     });
 });
 
-io.sockets.on('connection', function(socket){
-	socket.on('new_user', function(name, data){
-			if (name in users){
-				data(false);
-			}else{
-				data(true);
-			socket.nickname = name;
-			users[socket.nickname] = socket;
-			console.log('Add UserName : ' + name);
-			updateNickNames();
-		}
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+var conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'webdata'
+});
 
-	});
+fs.readFile('./public/tracking/index.html', function(err, data) {
+    if (err) {
+        throw err;
+    }
+    app.get('/public/tracking/cloud/register', function(req, res) {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        res.end();
+    });
+});
 
-	function updateNickNames(){
-		io.sockets.emit('user_names', Object.keys(users));
-	}
-	socket.on('open_chatbox', function(data){
-		users[data].emit('openbox', {nick: socket.nickname});
-	});
-	socket.on('send_message',function(data, sendto){
-		users[sendto].emit('new_message',{msg: data, nick: socket.nickname, sendto: sendto});
-		users[socket.nickname].emit('new_message',{msg: data, nick: socket.nickname, sendto: sendto});
+app.post('/cloud/register', function(req, res) {
+    conn.query("INSERT INTO project (id, project_name, is_del) VALUES (NULL, '" + req.body.project + "', '0')", function() {
+        conn.query('SELECT id FROM project WHERE project_name = "' + req.body.project + '"', function(error, data) {
+            conn.query("INSERT INTO user_data (id, full_name, badge_id, mail, phone, project_id, pm_mail, is_del) VALUES (NULL, '" + req.body.full_name + "', '" + req.body.bage_id + "', '" + req.body.email + "', '" + req.body.phone + "', '" + data[0].id + "', '" + req.body.pm_email + " ', '0')", function(error, data) {
+                conn.query("SELECT id, project_id FROM user_data WHERE badge_id = '" + req.body.bage_id + "'", function(error, data) {
+                    console.log(data);
+                    console.log("                ");
+                    var tmp1 = data[0].project_id;
+                    var tmp2 = data[0].id;
+                    var quota = [7, 1, 1, 512, 80];
+                    if (req.body.quota != 'default') {
+                        quota[0] = req.body.life_time;
+                        quota[1] = req.body.instance;
+                        quota[2] = req.body.cpu;
+                        quota[3] = req.body.ram;
+                        quota[4] = req.body.hdd;
+                    }
+                    conn.query("INSERT INTO detail_quota (id, project_id, life_time, instance, cpu, hdd, ram, user_id, status_id, note) VALUES (NULL, '" + data[0].project_id + "', '" + quota[0] + "', '" + quota[1] + "', '" + quota[2] + "', '" + quota[4] + "', '" + quota[3] + "', '" + data[0].id + "', '1', '" + req.body.note + "')", function(error, data) {
+                        console.log(data);
+                        console.log("                ");
+                        //conn.query("SELECT * FROM detail_quota WHERE  project_id = '" + tmp1 + "' AND user_id = '" + tmp2 + "'", function(error, data){
+                        //console.log(data);
+                        //console.log("                ");
+                        //});
+                        conn.query("SELECT * FROM detail_quota WHERE user_id = '" + tmp2 + "'", function(error, data) {
+                            console.log(data);
+                            console.log("                ");
+                        });
+                        conn.query("SELECT id FROM detail_quota WHERE user_id = '" + tmp2 + "'", function(error, data) {
+                            res.redirect('http://' + ip + '/neon/test02.php?id=' + data[0].id);
+                        });
+                    });
+                });
+            });
+        });
+    });
+});
 
-		console.log(data);
-	});
+io.sockets.on('connection', function(socket) {
+    socket.on('new_user', function(name, data) {
+        if (name in users) {
+            data(false);
+        } else {
+            data(true);
+            socket.nickname = name;
+            users[socket.nickname] = socket;
+            console.log('Add UserName : ' + name);
+            updateNickNames();
+        }
 
-	// Process for BOT if getting command from topic
-		socket.on('send_message_bot',function(data, sendto){
-		var sessionID = users[socket.nickname].id;
-		if ("" + user_list[sessionID] == 'undefined' ) {
-			var tmp = {};
+    });
+
+    function updateNickNames() {
+        io.sockets.emit('user_names', Object.keys(users));
+    }
+    socket.on('open_chatbox', function(data) {
+        users[data].emit('openbox', { nick: socket.nickname });
+    });
+    socket.on('send_message', function(data, sendto) {
+        users[sendto].emit('new_message', { msg: data, nick: socket.nickname, sendto: sendto });
+        users[socket.nickname].emit('new_message', { msg: data, nick: socket.nickname, sendto: sendto });
+
+        console.log(data);
+    });
+
+    // Process for BOT if getting command from topic
+    socket.on('send_message_bot', function(data, sendto) {
+        var sessionID = users[socket.nickname].id;
+        if ("" + user_list[sessionID] == 'undefined') {
+            var tmp = {};
             user_list[sessionID] = tmp;
         }
-		console.log ("OBJECT: " + user_list[sessionID]);
-		if (typeof user_data[sessionID] !== 'undefined' && user_data[sessionID] !== null){
-			var nextMessage = user_data[sessionID];
-			user_list[sessionID][user_data[sessionID]] = data;
-			console.log("Here is the data for " + user_data[sessionID] + ':' + user_list[sessionID][user_data[sessionID]]);
-			console.log("Here is object Keys: " + Object.keys(user_list[sessionID]));
-			user_data[sessionID] = null;
-			//New Code
-			selectTopic(users[socket.nickname].id, nextMessage, function(sessionID){
-			getTopic(sessionID, function(){
-				if (session_topic[sessionID] == "Cloud"){
-					createMessage(cloudTopic.cloudTopic(nextMessage, user_list[sessionID]),sessionID, function(message, items){
-						users[socket.nickname].emit('new_message',{msg: message, items: items, nick: 'BOT', sendto: sendto});
-						//log
-						console.log("Here log: " + user_data[sessionID]);
-					})
-				}
-			})
-		});
-		}
-		else {
-				selectTopic(users[socket.nickname].id, data, function(sessionID){
-				getTopic(sessionID, function(){
-					if (session_topic[sessionID] == "Cloud"){
-						createMessage(cloudTopic.cloudTopic(data, user_list[sessionID]),sessionID, function(message, items){
-							users[socket.nickname].emit('new_message',{msg: message,items: items, nick: 'BOT', sendto: sendto});
-							//log
-							console.log("Here log: " + user_data[sessionID]);
-						})
-					}
-				})
-			});
-			console.log("REC MESSAGE FROM: " + socket.nickname);
-			if(data.indexOf("what is my topic?") != -1){
-				console.log("Current topic of " + socket.nickname + " is: " + session_topic[users[socket.nickname].id] );
-				users[socket.nickname].emit('new_message',{msg: session_topic[users[socket.nickname].id] , nick: 'BOT', sendto: sendto});
-			}
-			else {
-				contactToAIAPI(data, function(){
-				if (AIData.result.action == "smalltalk.greetings.hello"){
-					users[socket.nickname].emit('new_message',{msg: AIMessage + " " + socket.nickname +"! What do you want to talk? " + "<button class = \"button5\" type=\"button\" name = \"res_button\" onclick=\"clickOnRes(this.innerHTML);this.disabled=true;\">Automation</button>" + " " + "<button class = \"button5\" type=\"button\" name = \"res_button_1\" onclick=\"clickOnRes(this.innerHTML)\">Cloud</button>", nick: 'BOT', sendto: sendto});
-				}
-				//else users[socket.nickname].emit('new_message',{msg: AIMessage, nick: 'BOT', sendto: sendto});
-			});
-			}
-		}
+        console.log("OBJECT: " + user_list[sessionID]);
+        if (typeof user_data[sessionID] !== 'undefined' && user_data[sessionID] !== null) {
+            var nextMessage = user_data[sessionID];
+            user_list[sessionID][user_data[sessionID]] = data;
+            console.log("Here is the data for " + user_data[sessionID] + ':' + user_list[sessionID][user_data[sessionID]]);
+            console.log("Here is object Keys: " + Object.keys(user_list[sessionID]));
+            user_data[sessionID] = null;
+            //New Code
+            selectTopic(users[socket.nickname].id, nextMessage, function(sessionID) {
+                getTopic(sessionID, function() {
+                    if (session_topic[sessionID] == "Cloud") {
+                        createMessage(cloudTopic.cloudTopic(nextMessage, user_list[sessionID]), sessionID, function(message, items) {
+                            users[socket.nickname].emit('new_message', { msg: message, items: items, nick: 'BOT', sendto: sendto });
+                            //log
+                            console.log("Here log: " + user_data[sessionID]);
+                        })
+                    }
+                })
+            });
+        } else {
+            selectTopic(users[socket.nickname].id, data, function(sessionID) {
+                getTopic(sessionID, function() {
+                    if (session_topic[sessionID] == "Cloud") {
+                        createMessage(cloudTopic.cloudTopic(data, user_list[sessionID]), sessionID, function(message, items) {
+                            users[socket.nickname].emit('new_message', { msg: message, items: items, nick: 'BOT', sendto: sendto });
+                            //log
+                            console.log("Here log: " + user_data[sessionID]);
+                        })
+                    }
+                })
+            });
+            console.log("REC MESSAGE FROM: " + socket.nickname);
+            if (data.indexOf("what is my topic?") != -1) {
+                console.log("Current topic of " + socket.nickname + " is: " + session_topic[users[socket.nickname].id]);
+                users[socket.nickname].emit('new_message', { msg: session_topic[users[socket.nickname].id], nick: 'BOT', sendto: sendto });
+            } else {
+                contactToAIAPI(data, function() {
+                    if (AIData.result.action == "smalltalk.greetings.hello") {
+                        users[socket.nickname].emit('new_message', { msg: AIMessage + " " + socket.nickname + "! What do you want to talk? " + "<button class = \"button5\" type=\"button\" name = \"res_button\" onclick=\"clickOnRes(this.innerHTML);this.disabled=true;\">Automation</button>" + " " + "<button class = \"button5\" type=\"button\" name = \"res_button_1\" onclick=\"clickOnRes(this.innerHTML)\">Cloud</button>", nick: 'BOT', sendto: sendto });
+                    }
+                    //else users[socket.nickname].emit('new_message',{msg: AIMessage, nick: 'BOT', sendto: sendto});
+                });
+            }
+        }
 
-	});
+    });
 
-	socket.on('disconnect', function(data){
-		if (!socket.nickname) return;
-		delete users[socket.nickname];
-		updateNickNames();
-	});
+    socket.on('disconnect', function(data) {
+        if (!socket.nickname) return;
+        delete users[socket.nickname];
+        updateNickNames();
+    });
 });
 
 //contact to API AI
 function contactToAIAPI(messageAI, cb) {
-	var ai_res_message = null;
-	var request = appAI.textRequest(messageAI, {
-    	sessionId: '12345676111'
-	});
- 
-	request.on('response', function(response) {
-    	//console.log(response);
-		AIData = response;
-    	ai_res_message = response.result.fulfillment.speech;
-		//ai_res_message = response.syscall;
-    	console.log(ai_res_message);
-    	AIMessage = ai_res_message;
+    var ai_res_message = null;
+    var request = appAI.textRequest(messageAI, {
+        sessionId: '12345676111'
+    });
+
+    request.on('response', function(response) {
+        //console.log(response);
+        AIData = response;
+        ai_res_message = response.result.fulfillment.speech;
+        //ai_res_message = response.syscall;
+        console.log(ai_res_message);
+        AIMessage = ai_res_message;
         cb(ai_res_message);
         console.log("Message tmp input: " + AIMessage);
-	});
-	request.on('error', function(error) {
-    	console.log(error);
-	});
-	request.end();
+    });
+    request.on('error', function(error) {
+        console.log(error);
+    });
+    request.end();
 }
 
 // assign the topic for user
 function selectTopic(sessionID, message, cb) {
-	if (message == "Automation") session_topic[sessionID] = "Automation";
-	if (message == "Cloud") session_topic[sessionID] = "Cloud";
-	cb(sessionID);
+    if (message == "Automation") session_topic[sessionID] = "Automation";
+    if (message == "Cloud") session_topic[sessionID] = "Cloud";
+    cb(sessionID);
 }
 
 // get the topic of user
 function getTopic(sessionID, cb) {
-	var topic = session_topic[sessionID];
-	cb();
+    var topic = session_topic[sessionID];
+    cb();
 }
 
 // create message
 function createMessage(buttonName, sessionID, cb) {
-	var command = buttonName.command;
-	user_data[sessionID] = command;
-	var promButton = "";
-	if (buttonName.buttonName != "NA"){
-			for(var i = 0; i < handleButton(buttonName).length;i++){
-		console.log("Button Name : " + handleButton(buttonName)[i]);
-        promButton = promButton + "<button class = \"button5\" type=\"button\" name = \"res_button\" onclick=\"clickOnRes(this.innerHTML)\">" + handleButton(buttonName)[i] + "</button>";
-  		}
-	}
-	else promButton = "NA";
+    var command = buttonName.command;
+    user_data[sessionID] = command;
+    var promButton = "";
+    if (buttonName.buttonName != "NA") {
+        for (var i = 0; i < handleButton(buttonName).length; i++) {
+            console.log("Button Name : " + handleButton(buttonName)[i]);
+            promButton = promButton + "<button class = \"button5\" type=\"button\" name = \"res_button\" onclick=\"clickOnRes(this.innerHTML)\">" + handleButton(buttonName)[i] + "</button>";
+        }
+    } else promButton = "NA";
 
-	cb(buttonName.message, promButton);
+    cb(buttonName.message, promButton);
 }
 
 // handle button
 function handleButton(buttonName) {
-	if (buttonName.buttonName != "NA") return buttonName.buttonName.split(" andButton ");
-	else return "NA";
+    if (buttonName.buttonName != "NA") return buttonName.buttonName.split(" andButton ");
+    else return "NA";
 }
---
+++ AutomationBotChat/public/tracking/js/index.js
@@ -0,0 +1,188 @@
+ $(document).ready(function() {
+       var setRecommandValues = function() {
+       $('[name="life_time"]').val(7);
+       $('[name="instance"]').val(1);
+       $('[name="cpu"]').val(1);
+       $('[name="ram"]').val(512);
+       $('[name="hdd"]').val(80);
+
+       $('[name="life_time"]').prop("disabled", true);
+       $('[name="instance"]').prop("disabled", true);
+       $('[name="cpu"]').prop("disabled", true);
+       $('[name="ram"]').prop("disabled", true);
+       $('[name="hdd"]').prop("disabled", true);
+    }
+
+    var setRecommandText = function() {
+       $('[name="life_time"]').innerHTML() = 7;
+       $('[name="instance"]').innerHTML() = 1;
+       $('[name="cpu"]').innerHTML() = 1;
+       $('[name="ram"]').innerHTML() = 512;
+       $('[name="hdd"]').innerHTML() = 80;
+
+       $('[name="life_time"]').prop("disabled", true);
+       $('[name="instance"]').prop("disabled", true);
+       $('[name="cpu"]').prop("disabled", true);
+       $('[name="ram"]').prop("disabled", true);
+       $('[name="hdd"]').prop("disabled", true);
+    }
+
+       var removeRecommandValues = function() {   
+       $('[name="life_time"]').prop("disabled", false);
+       $('[name="instance"]').prop("disabled", false);
+       $('[name="cpu"]').prop("disabled", false);
+       $('[name="ram"]').prop("disabled", false);
+       $('[name="hdd"]').prop("disabled", false);
+     }
+
+     $('[value="default"]').click(setRecommandText)   
+    $('[value="custom"], [value="advance"]').click(removeRecommandValues)
+    // setRecommandText();
+    $('[value="advance"]').click(function(){
+      $('#advance-quotas').show();
+    });
+    $('[value="default"], [value="custom"]').click(function(){
+      $('#advance-quotas').hide();
+    });
+    
+    $('#contact_form').bootstrapValidator({
+        // To use feedback icons, ensure that you use Bootstrap v3.1.0 or later
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+            full_name: {
+                validators: {
+                        stringLength: {
+                        min: 2,
+                    },
+                        notEmpty: {
+                        message: 'Please supply your name'
+                    }
+                }
+            },
+            bage_id: {
+                validators: {
+                    notEmpty: {
+                        message: 'Please supply your Badge ID'
+                    },
+                    stringLength: {
+                        min: 6,
+                        max: 6,
+                        message: 'Please supply a valid Badge ID'
+                    },
+                    
+                }
+            },
+            email: {
+                validators: {
+                    notEmpty: {
+                        message: 'Please supply your email address'
+                    },
+                    emailAddress: {
+                        message: 'Please supply a valid email address'
+                    }
+                }
+            },
+            phone: {
+                validators: {
+                    stringLength: {
+                        min: 4,
+                        message: 'Please supply a vaild phone number'
+                    },
+                    notEmpty: {
+                        message: 'Please supply your phone number'
+                    },
+                }
+            },
+            project: {
+                validators: {
+                     stringLength: {
+                        min: 4,
+                    },
+                    notEmpty: {
+                        message: 'Please supply your project'
+                    }
+                }
+            },
+            pm_email: {
+                validators: {
+                    notEmpty: {
+                        message: 'Please supply your PM email address'
+                    },
+                    emailAddress: {
+                        message: 'Please supply a valid email address'
+                    }
+                }
+            },
+            life_time: {
+                validators: {
+                    notEmpty: {
+                        message: 'Please supply the period of time you want to deploy'
+                    },
+                }
+            },
+            instance: {
+                validators: {
+                    notEmpty: {
+                        message: 'Please supply the number of instance you want to deploy'
+                    },
+                }
+            },
+            cpu: {
+                validators: {
+                    notEmpty: {
+                        message: 'Please supply the amount of CPU you want to deploy'
+                    },
+                }
+            },
+            ram: {
+                validators: {
+                    notEmpty: {
+                        message: 'Please supply the amount of RAM you want to use'
+                    },
+                }
+            },
+           hdd: {
+                validators: {
+                    notEmpty: {
+                        message: 'Please supply the amount of disk you want to use'
+                    },
+                }
+            },
+
+            note: {
+                validators: {
+                      stringLength: {
+                        min: 4,
+                        max: 1000,
+                        message:'Please enter at least 4 characters and no more than 1000'
+                    },
+                    }
+                }
+            }
+        });
+        .on('success.form.bv', function(e) {
+            $('#success_message').slideDown({ opacity: "show" }, "slow") // Do something ...
+                $('#contact_form').data('bootstrapValidator').resetForm();
+
+            // Prevent form submission
+            e.preventDefault();
+
+            // Get the form instance
+            var $form = $(e.target);
+
+            // Get the BootstrapValidator instance
+            var bv = $form.data('bootstrapValidator');
+
+            // Use Ajax to submit form data
+            $.post($form.attr('action'), $form.serialize(), function(result) {
+                console.log(result);
+            }, 'json');
+        });
+});
+
+
+
